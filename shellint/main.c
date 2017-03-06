bcf4f2858e1eae3ecb1f100cc35f0c85634954f4
Ken Luo <luozhirong@gmail.com>
2017-03-06 10:34:29 -0800

    3 line exit

--- shellint/main.c
+++ shellint/main.c
@@ -1,62 +1,43 @@
-//
 //  main.c
 //  shellint
 //
 //  Created by Phillip Gulegin on 2/23/17.
 //  Copyright © 2017 Phillip Gulegin. All rights reserved.
 //
-
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
-
-// maximum length of a command
 #define MAXLINE 80
-
 #define TOKEN_BUFFER_SIZE 80
 #define TOKEN_DELIMITER "\n"
-
+// keeps track of the last 10 history commands
+char history[10][MAXLINE];
 char **arg_parser(char *input)
 {
     int bufferSize = TOKEN_BUFFER_SIZE, position = 0;
-
+    
     char **args = malloc(bufferSize * sizeof(char*));
     char *arg;
-
+    
     arg = strtok(input, TOKEN_DELIMITER);
     while (arg != NULL) {
         args[position] = arg;
         position++;
-
+        
         if (position >= bufferSize) {
             bufferSize += TOKEN_BUFFER_SIZE;
             args = realloc(arg, bufferSize * sizeof(char*));
         }
-
+        
         arg = strtok(NULL, TOKEN_DELIMITER);
     }
     args[position] = NULL;
     return args;
 }
-
-void history(char **hist, int current){
-    //    int i = current;
-    //    int histNum = 1;
+void addHistory(char *input) {
     
-    //char *histA[MAXLINE];
-    //    free(histA);
-    
-    //    for(int temp = 0; temp < MAXLINE; temp++){
-    //      strcpy(histA, hist[1]);
-    //    }
-    //    while(1){
-    //        if(&hist){
-    //            printf("%d %s\n", histNum, &hist);
-    //            histNum++;
-    //        }
-    //    }
-
+}
 // checks if input contains &
 int containsAmp(char input[]) {
     
@@ -70,54 +51,63 @@ int containsAmp(char input[]) {
     
     return 0;
 }
-
-
-
-/*****************************************************
-//Parse user input
-
-int analyzeInput(char input[], char *args[]) {
+// checks if command is a history command
+// return 1:  if command is: '!!'
+// return 0:  command is: '!N'
+// return -1: command is: 'history'
+// return -2: command is: 'exit'
+// return 99: history is not the command
+/* int containsHistOrExit(char input[]) { */
     
-    int index = -1;
-    int count = 0;
+/*     if (input[0] == '!' && input[1] == '!') { */
+/*         return 1; */
+/*     } */
+/*     else if (input[0] == '!' && (input[1] >= 48 && input[1] <= 57)) { */
+/*         return 0; */
+/*     } */
+/*     // I know, I know... Lol. */
+/*     else if (input[0] == 'h' && */
+/*              input[1] == 'i' && */
+/*              input[2] == 's' && */
+/*              input[3] == 't' && */
+/*              input[4] == 'o' && */
+/*              input[5] == 'r' && */
+/*              input[6] == 'y') { */
+/*         return -1; */
+/*     } */
+/*     else if (input[0] == 'e' && */
+/*              input[1] == 'x' && */
+/*              input[2] == 'i' && */
+/*              input[3] == 't' ) { */
+/*         return -2; */
+/*     } */
+/*     else { */
+/*         return 99; */
+/*     } */
+/* } */
+// removes '&' from input
+void removeAmp(char* str) {
     
-    unsigned long length = strlen(input);
+    //int currArrLen = sizeof(str) / sizeof(str[0]);
+    unsigned long len = strlen(str);
+    int indexToDelete;
+    int i = 0;
     
-//    printf("%lu", length);
-    
-    for(int i = 0; i < length; i++) {
-        
-        switch(input[i]){
-            case ' ':
-                break;
-            case '\n':
-                if(index != -1){
-                    args[count] = &input[index];
-                    count++;
-                }
-                input[i] = '\0';
-//                args[count] = NULL;
-                break;
-            default:
-//                if(index == -1){
-                    index = i;
-//                }
-                
-//                if(input[i] == '&'){
-//                    input[i] = '\0';
-//                }
+    for(i = 0; i < len; i++) {
+        if(str[i] == '&') {
+            indexToDelete = i;
+            break;
         }
     }
     
+    memmove(&str[indexToDelete], &str[indexToDelete + 1], strlen(str) - indexToDelete);
     
     
-    return 0;
+    
+    //printf("%s", str);
     
 }
-************************************************************/
-
-
-
+// main function
 int main(void) {
     
     char input[MAXLINE];            // inital input
@@ -133,74 +123,91 @@ int main(void) {
         // flushes all output
         fflush(stdout);
         
-        // steps from CSNS
-        /** * After reading user input, the steps are:
-         
-         * (1) fork a child process using fork()
-         
-         * (2) the child process will invoke execvp()
-         
-         * (3) if command included &, parent will invoke wait()
-         
-         */
-
-
-        // grabs in the input and the length of the input
+        // gets the input
         fgets(input, MAXLINE, stdin);
-
-
-        /**************************
-        // analyze the input first
-        analyzeInput(input, args);
-
-         ***********************/
-
-        args = arg_parser(input);
-
-
-        // create a new child process
-	/* pid = fork(); */
         
-        
-        /* // true: then we want child and parent to execute */
-        /* // false: then we want the parent to wait for the child to execute */
-        /* if (containsAmp(input)) { */
-            
-        /*     // executes if we're the child process */
-        /*     if (pid == 0) { */
-        /*         printf("1\n"); */
-        /*         execvp(args[0], args); */
-        /*         exit(0); */
-        /*     } */
-        /*     // executes if we're the parent process */
-        /*     else if (pid > 0) { */
-        /*         printf("2\n"); */
-        /*     } */
-        /*     // executes if we're the parent process, but child couldn't be created */
-        /*     else { */
-        /*         printf("3\n"); */
-        /*     } */
+        // if the command is a history command -- 3 scenarios
+        /* switch (containsHistOrExit(input)) { */
+                
+        /*         // executes if command is: '!!' -> execute most recent command */
+        /*     case 1: */
+                
+        /*         break; */
+                
+        /*         // executes if command is: '!N' -> execute Nth command */
+        /*     case 0: */
+                
+        /*         break; */
+                
+        /*         // executes if command is: 'history' -> print recent history to console */
+        /*     case -1: */
+                
+        /*         // return to the beginning of a while loop */
+        /*         continue; */
+                
+        /*         // execute if command is exit */
+        /*     case -2: */
+        /*         should_run = 0; */
+        /*         continue; */
         /* } */
-        /* else { */
+        
+        
+        // create a new child process
+        pid = fork();
+        
+        // true: then we want child and parent to execute
+        // false: then we want the parent to wait for the child to execute
+        if (containsAmp(input)) {
             
-        /*     // executes if we're the child process */
-        /*     if (pid == 0) { */
-        /*         printf("4\n"); */
-        /*         execvp(args[0], args); */
-        /*         exit(0); */
-        /*     } */
-        /*     // executes if we're the parent process */
-        /*     else if (pid > 0) { */
-        /*         wait(NULL); */
-        /*         printf("5\n"); */
-        /*     } */
-        /*     // executes if we're the parent process, but child couldn't be created */
-        /*     else { */
-        /*         printf("6\n"); */
-        /*     } */
-	//        }
+            // removes the '&' from the input
+            removeAmp(input);
+            
+            // returns a set of arguments
+            args = arg_parser(input);
+            
+            // executes if we're the child process
+            if (pid == 0) {
+                //                execvp(args[0], args);
+                if (execvp(args[0], args) == -1)
+                {
+                    printf("Command not executed correctly.\n");
+                    exit(0);
+                }
+                
+            }
+            // executes if we're the parent process
+            else if (pid > 0) {
+                // do nothing with the parent because we want them to run together
+            }
+            // executes if we're the parent process, but child couldn't be created
+            else {
+                printf("Something went wrong.\n");
+            }
+        }
+        else {
+            
+            // returns a set of arguments
+            args = arg_parser(input);
+            
+            // executes if we're the child process
+            if (pid == 0) {
+                execvp(args[0], args);
+            }
+            // executes if we're the parent process
+            else if (pid > 0) {
+                wait(NULL);
+            }
+            // executes if we're the parent process, but child couldn't be created
+            else {
+                printf("Something went wrong.\n");
+            }
+        }
+	if(input[strlen(input) - 1] == '\n')
+	  input[strlen(input) -1] = '\0';
+
+	if(strcmp(input, "exit") == 0)
+	  break;
     }
     
     return 0;
 }
-
